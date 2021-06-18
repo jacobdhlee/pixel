4865dc4d72dce470d790480e0482fba30cd25e33
Jacob Lee <jacoblee@Jacobs-MacBook-Air.local>
2021-06-18 11:42:36 -0400

    add input enter press search

--- components/Search/index.js
+++ components/Search/index.js
@@ -5,12 +5,16 @@ import { SearchContainer, Input, InputWrapper, Button } from './styles';
 
 const Search = ({onClick, searchPic}) => {
     const [ inputVal, changeInput ] = useState('')
-
+    const handleKeyPress = (e) => {
+        if(e.key === 'Enter') {
+            onClick(inputVal);
+        }
+    }
     return (
         <SearchContainer url={searchPic?.src?.original}>
             <h2>Please search or browse all stock photos</h2>
             <InputWrapper>
-                <Input type={'text'} value={inputVal} onChange={(e) => changeInput(e.target.value)}/>
+                <Input type={'text'} value={inputVal} onChange={(e) => changeInput(e.target.value)} onKeyPress={handleKeyPress}/>
                 <Button onClick={() => onClick(inputVal)}><FaSearch /></Button>
             </InputWrapper>
             <h5><strong>Suggested:</strong> coffee, cafe, more</h5>
